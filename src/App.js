d6ae3d383e94b3485dbb1a64393f469a58405933
Aske21 <veledarevica@yahoo.com>
2021-12-10 00:58:33 +0100

    added sign in with google

--- src/App.js
+++ src/App.js
@@ -6,22 +6,32 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Login from "./components/Login";
+
 function App() {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <div className="App">
       <Router>
-        <>
-          <Header />
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
 
-          <AppBody>
-            <Sidebar />
-            <Switch>
-              <Route path="/">
-                <Chat />
-              </Route>
-            </Switch>
-          </AppBody>
-        </>
+            <AppBody>
+              <Sidebar />
+              <Switch>
+                <Route path="/">
+                  <Chat />
+                </Route>
+              </Switch>
+            </AppBody>
+          </>
+        )}
       </Router>
     </div>
   );
